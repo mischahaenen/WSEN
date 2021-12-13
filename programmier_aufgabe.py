c8a92e470af81374a9b5831ba6d1ed0f365c4553
mischahaenen <mischa.j.haenen@gmail.com>
2021-12-13 15:37:00 +0100

    initial commit

--- programmier_aufgabe.py
+++ programmier_aufgabe.py
@@ -33,14 +33,16 @@ class SparseVector():
                 vector.append(0)
         return vector 
     # überprüft, ob index i kleiner als dimension und positiv ist, dann Vektorelement mit index i, diesen Wert(value) erhalten
+    #TODO: Name ändern, verstehe setNonzeroValue(value, index) nicht ist es setter oder getter?
     def getNonzeroValueByIndex(self, index: int):
-        if (self.dimension < index or index < 0):
+        if (self.dimension <= index or index < 0):
             return None
         if (index in self.nonzeroIndexes):
             i = self.nonzeroIndexes.index(index)
             return self.nonzeroValues[i]
         else:
             return None
+        
     #  multipliziert den Vektor mit einem factor
     def multiply(self, factor: float):
         multiplied_vector = []
@@ -48,15 +50,13 @@ class SparseVector():
             multiplied_vector.append(value * factor)
         return multiplied_vector
 
-
-
 # welche aus den zwei Sparse-Vektoren a und b die Summe bildet und diesen als neues Sparse-Objekt zurückgibt.
 def addSparse(sparse_vector1: SparseVector, sparse_vector2: SparseVector) -> SparseVector:
     summed_vector = list(map(lambda x, y: x + y, sparse_vector1.getVectorWithZeroValues(), sparse_vector2.getVectorWithZeroValues()))
     non_zero_indexes = []
     non_zero_values = []
     for index, value in enumerate(summed_vector):
-        if (value is not 0):
+        if (value != 0):
             non_zero_indexes.append(index)
             non_zero_values.append(value)
     return SparseVector(len(summed_vector), non_zero_values, non_zero_indexes) 
@@ -71,6 +71,7 @@ def dotSparse(sparse_vector1: SparseVector, sparse_vector2: SparseVector) -> int
     zipped_vector = zip(vector1, vector2)
     return sum(i[0] * i[1] for i in zipped_vector)
 
+# TODO: können parameter auch anders benannt werden? 
 # die ein Objekt der Klasse SparseVektor mit den folgenden Eigenschaften erzeugt
 # Dimension n, höchstens m von Null verschiedene Einträge, deren Wert im Intervall [a,b] liegen.
 # Diese Einträge sind zufällig im ganzen Vektor verteilt.
@@ -90,12 +91,29 @@ def createRandomSparse(dimension: int, max_non_zero_values: int, start: int, end
 sparse1 = SparseVector(10, [1,2,3,4], [2,3,4,5])
 sparse2 = SparseVector(10, [1,5,6,8], [0,1,4,9])
 # Tests
+print('-----------------------')
+print('Sparse1')
+print('-----------------------')
 print(sparse1)
 print('Dimension: ', sparse1.getDimension())
 print('Non-Zero-Indexes: ', sparse1.getNonzeroIndexes())
 print('Non-Zero-Values', sparse1.getNonzeroValues())
-print('Value vom Vektor wenn Index existiert: ', sparse1.getNonzeroValueByIndex(2))
+print('Value wenn Index 6 existiert : ', sparse1.getNonzeroValueByIndex(6))
 print('Multipliziert Sparse1', sparse1.multiply(3))
+print('-----------------------')
+print('Sparse2')
+print('-----------------------')
+print(sparse2)
+print('Dimension: ', sparse2.getDimension())
+print('Non-Zero-Indexes: ', sparse2.getNonzeroIndexes())
+print('Non-Zero-Values', sparse2.getNonzeroValues())
+print('Value wenn Index 4 existiert : ', sparse2.getNonzeroValueByIndex(4))
+print('Multipliziert Sparse2', sparse2.multiply(9))
+print('-----------------------')
+print('Berechnungen Sparse 1 & Sparse 2')
+print('-----------------------')
+print('Summe Sparse 1 & 2:')
 print(addSparse(sparse1, sparse2))
-print('Skalarprodukt ', dotSparse(sparse1, sparse2))
-print(createRandomSparse(10, 2, 1, 6))
+print('Skalarprodukt Spare 1 & 2:', dotSparse(sparse1, sparse2))
+print('Random Sparse n=10, m=5, a=1, b=6:')
+print(createRandomSparse(10, 5, 1, 6))
